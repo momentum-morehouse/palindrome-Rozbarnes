1cdeac47b0a19cd891915b2a7e587930dfb8cdf5
Roz Barnes <rozbarnes@gmail.com>
2020-06-24 15:09:43 +0000

    attempted the homework

---
+++ main.py
@@ -0,0 +1,10 @@
+#write a program that asks for the user inputs a sentence
+print("Hello!")
+#get info from user Ques 1
+name = input('Please enter your name ')
+print(name)
+if(name == name[:]):
+ask_text input = ('Please type text ')
+
+
+#A Santa at Nasa
